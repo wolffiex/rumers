5bb18f6c7436b711e34d1e261640f2779170132c
Adam Wolff <awolff@gmail.com>
2021-02-13 14:50:40 -0800

    getching

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
+extern crate pancurses;
+
+use pancurses::{initscr, endwin, Input, noecho, cbreak};
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 use std::thread;
-use crate::InputState::Ready;
+use crate::InputState::{Ready, Quitting};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum InputState {
     Ready,
     Labeling,
+    Quitting,
 }
+
 #[derive(Debug)]
 struct State {
     timers: Vec<(f64, Option<f64>)>,
@@ -27,7 +32,7 @@ fn main() {
     let worker = thread::spawn(move || {
         let id = 77;
         let state = State {
-            timers: vec!(( 1.0, None)),
+            timers: vec!((1.0, None)),
             input_state: Ready,
         };
         // The thread takes ownership over `thread_tx`
@@ -39,9 +44,35 @@ fn main() {
         println!("thread {} finished", id);
     });
 
-    let getch = rx.recv();
+    let window = initscr();
+    window.keypad(true);
+    noecho();
+    cbreak();
+    window.printw("Hello Rust");
+    window.refresh();
+    window.timeout(250);
+    let mut input_state = Ready;
+    let mut i = 0;
+    while input_state != Quitting {
+        let k = window.getch();
+        match k {
+            Some(Input::Character('q')) => { input_state = Quitting }
+            Some(input) => { window.addstr(&format!("{:?}", input)); }
+            Some(_) => {}
+            None => {
+                if i % 10 == 0 {
+                    window.addstr(format!("i {}", i));
+                    ()
+                }
+                i = i +1;
+            }
+        }
+    }
+    endwin();
+
+    let view_model = rx.recv();
+    println!("{:?}", view_model);
     worker.join().expect("oops! the child thread panicked");
 
     // Show the order in which the messages were sent
-    println!("{:?}", getch);
 }
