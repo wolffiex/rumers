513164e3ac6f2b5d3c429ef440bf4b16ccd6e211
Adam Wolff <adam@itsy.localdomain>
2021-02-17 19:12:46 -0800

    Countdown

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 extern crate pancurses;
 
 use pancurses::{initscr, endwin, Input, noecho, cbreak, Window};
-use std::time::SystemTime;
+use std::time::{SystemTime, Duration};
+use std::cmp;
+use std::alloc::System;
 
 mod font;
 
@@ -27,10 +29,10 @@ fn main() {
     while render(&window, state) {
         state = match window.getch() {
             Some(input) => match state {
-                State::Starting(minutes) => adjust_timer(minutes, input),
-                State::Running(end_time) => while_running(end_time, input),
+                State::Starting(minutes) => setup_mode(minutes, input),
+                State::Running(end_time) => run_mode(end_time, input),
                 State::Paused(end_time, pause_time) =>
-                    while_paused(end_time, pause_time, input)
+                    pause_mode(end_time, pause_time, input)
             }
             None => state
         }
@@ -38,30 +40,49 @@ fn main() {
     endwin();
 }
 
-fn while_running(end_time: SystemTime, input: Input) -> State {
-    State::Running(end_time)
-}
-
-fn while_paused(end_time: SystemTime, paused_time: SystemTime, input: Input) -> State {
-    State::Paused(end_time, paused_time)
-}
-
-fn adjust_timer(minutes: usize, input: Input) -> State {
+fn setup_mode(minutes: usize, input: Input) -> State {
     match input {
-        Input::KeyEnter => State::Running(SystemTime::now()),
+        Input::Character(' ') | Input::KeyEnter => run_state(minutes),
         Input::KeyUp => State::Starting(minutes + 1),
-        Input::KeyDown => State::Starting(minutes - 1),
+        Input::KeyDown => State::Starting(if minutes > 0 { minutes - 1 } else { 0 }),
         _ => State::Starting(minutes),
+    }
+}
+
+fn run_state(minutes: usize) -> State {
+    State::Running(
+        SystemTime::now().checked_add(
+            Duration::from_secs((minutes * 60) as u64)).unwrap())
+}
+
+
+fn run_mode(end_time: SystemTime, input: Input) -> State {
+    match input {
+        Input::Character(' ') => State::Paused(end_time, SystemTime::now()),
+        _ => State::Running(end_time)
+    }
+}
+
+fn pause_mode(end_time: SystemTime, paused_time: SystemTime, input: Input) -> State {
+    match input {
+        Input::Character(' ') => State::Running(SystemTime::now()),
+        _ => State::Paused(end_time, paused_time)
     }
 }
 
 fn render(window: &Window, state: State) -> bool {
     let (minutes, seconds) = match state {
         State::Starting(minutes) => (minutes, 0 as usize),
-        State::Running(end_time) => (11, 12),
+        State::Running(end_time) => min_sec_until(end_time),
         State::Paused(end_time, pause_time) => (13, 14),
     };
     window.clear();
     window.printw(format!("{}:{}", minutes, seconds));
     true
+}
+
+fn min_sec_until(end_time: SystemTime) -> (usize, usize) {
+    let duration = end_time.duration_since(SystemTime::now()).unwrap();
+    let secs = duration.as_secs() as usize;
+    (secs / 10, secs % 60)
 }
